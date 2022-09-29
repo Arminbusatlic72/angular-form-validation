5e6a16490a00ae5cce30d28e42f6fa8c36b2dfe0
Armin <busatar@cronos.be>
2022-09-29 10:10:07 +0200

    Using @ViewChild to access template reference

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'form-validations';
   searchText: String = '';
+  @ViewChild('form') signupForm!:NgForm;
   loginUser(value:String) {console.log(value)}
   onSearchTextEntered(searchValue: String) {
     this.searchText = searchValue;
     console.log(this.searchText)
 
+  }
+  // onSubmit(form: NgForm) {
+  //   console.log(form.value.email)
+
+  // }
+  onSubmit() {
+    console.log(this.signupForm)
   }
 }
