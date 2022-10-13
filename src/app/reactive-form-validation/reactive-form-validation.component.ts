1f06ef1450fe1d01c7ed72efd4ac8861da18681a
Armin <busatar@cronos.be>
2022-10-13 15:51:23 +0200

    Adding validators

--- src/app/reactive-form-validation/reactive-form-validation.component.ts
+++ src/app/reactive-form-validation/reactive-form-validation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup , FormControl} from '@angular/forms';
+import { FormGroup , FormControl, Validators, FormArray} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form-validation',
@@ -11,18 +11,29 @@ export class ReactiveFormValidationComponent implements OnInit {
   genders: string[] = ['male', 'female']
   signupForm!: FormGroup;
   constructor() { }
-
+  addHobby() {
+    const controls = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(controls);
+  }
   ngOnInit(): void {
     this.signupForm = new FormGroup({
-      'username' : new FormControl(null),
-      'email' : new FormControl(null),
+      'userData' : new FormGroup( {
+      'username' : new FormControl(null, Validators.required),
+      'email' : new FormControl(null, [Validators.required, Validators.email]),
+      }),
       'gender' : new FormControl('male'),
+      'hobbies' : new FormArray([])
 
 
     });
+
   }
   onSubmit() {
-    console.log(this.signupForm)
+console.log(this.signupForm)
+
+  }
+  getControls() {
+    return (<FormArray>this.signupForm.get('hobbies')).controls;
   }
 
 }
